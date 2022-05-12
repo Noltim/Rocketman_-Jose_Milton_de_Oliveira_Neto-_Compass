345507527c01ffcf1d6ad4e138b4dba66e4de11e
Milton <milton_oliveira18@live.com>
2022-05-12 13:01:14 -0300

    Atualização de estudos

---
+++ mini-Projeto.js
@@ -0,0 +1,25 @@
+// minha resolução
+
+// let a = 'Vermelhor';
+// let b = 'Azul';
+
+// console.log(a);
+// console.log(b);
+
+// a = 'Azul';
+// b = 'Vermelho';
+
+// console.log(a);
+// console.log(b);
+
+//PS: Minha resolução foi pensanda no fator mutavel da
+//variavel por ser let
+
+//Resolução do video
+
+// let c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+// console.log(b);
