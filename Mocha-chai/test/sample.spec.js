9c42117942a3aa2e8a41d8e9b1e62dc8c45105cf
Milton <milton_oliveira18@live.com>
2022-05-17 21:46:26 -0300

    Atualização de estudos

---
+++ Mocha-chai/test/sample.spec.js
@@ -0,0 +1,29 @@
+import assert from "assert/strict";
+import chai from "chai";
+
+const expect = chai.expect;
+
+describe("Meu primeiro teste", function () {
+    it("Verificar a variável 'aux'", () => {
+        let aux = "teste"
+        // assert.strictEqual(aux, "teste")
+        expect(aux).to.be.equals('teste').and.to.be.a('string')
+    })
+    it("Verificar outra variavel", function(){
+        let aux = [1, 2, 3];
+        // assert.deepEqual(aux, [1, 2, 3])
+        expect(aux).to.deep.equal([1, 2, 3]).and.not.deep.equal([3, 2, 1])
+    })
+})
+describe("Meu segundo teste", function () {
+    it("Verificar uma divergencia", () => {
+        let aux = "teste"
+        // assert.notStrictEqual(aux, "fuleiro")
+        expect(aux).to.not.deep.equals('test')
+    })
+    it("Verificar uma divergencia", () => {
+        let aux = "teste"
+        // assert.notStrictEqual(aux, ["Cavalor, cobra, castro", 2, '$'])
+        expect(aux).to.not.deep.equals(["Cavalor, cobra, castro", 2, '$'])
+    })
+})
